ee6cc58e660019598134c7bc27ee36b886281b50
Jeyhun <creshidov23@gmail.com>
2021-10-14 14:14:23 +0400

    email sent

---
+++ app/Http/Controllers/Admin/EmailSentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Email;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\InvitationLetter;
+
+class EmailSentController extends Controller
+{
+    // public function index()
+    // {
+    //     foreach($registration as $reg){
+    //         $data['email'] = $reg['email'];
+    //         $data['fullname'] = $reg['firstname']. ' ' . $reg['lastname'];
+    //         $check = Email::where('email', $data['email'])->exists();
+    //         if(!$check){
+    //             $add = Email::create($data);
+    //             echo $add;
+    //         }
+    //     }
+    //     return "Successfully finished!"; 
+    // }
+
+    public function send()
+    {
+        $emails = Email::all();
+        foreach($emails as $email){
+            $mail = Mail::to($email->email)->send(new InvitationLetter());
+            return "Finished successfully";
+            echo $mail;
+        }
+        return "Finished successfully";
+    }
+}
---
+++ app/Mail/InvitationLetter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class InvitationLetter extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject("Herodoc'a dəvətlisiniz")->view('vendor.notifications.invitation_letter');
+    }
+}
